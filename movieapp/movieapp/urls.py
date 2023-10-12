d4f9b29201c26dbca01a9a12ee309eb1664d24eb
eResult <eresultolu@gmail.com>
2023-10-12 19:13:44 +0300

    First Launch

--- movieapp/movieapp/urls.py
+++ movieapp/movieapp/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+# http://127.0.0.1:8000/
+# http://127.0.0.1:8000/movies
+# http://127.0.0.1:8000/movies/3
+# http://127.0.0.1:8000/movies/walking-dead
+
+urlpatterns = [path("admin/", admin.site.urls), path("", include("movies.urls"))]

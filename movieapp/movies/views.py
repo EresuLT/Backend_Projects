b9b0055915216a092d871f0cc0397c5af99f3434
eResult <eresultolu@gmail.com>
2023-10-14 12:14:26 +0300

    Movie App Completed

--- movieapp/movies/views.py
+++ movieapp/movies/views.py
@@ -1,38 +1,6 @@
 from django.shortcuts import render
 from .models import Category, Movie
 
-movie_list = [
-    {
-        "id": 1,
-        "movie_name": "Movie 1",
-        "description": "Movie 1 description",
-        "image": "1stMovie.jpg",
-        "homepage": True,
-    },
-    {
-        "id": 2,
-        "movie_name": "Movie 2",
-        "description": "Movie 2 description",
-        "image": "2ndMovie.jpg",
-        "homepage": True,
-    },
-    {
-        "id": 3,
-        "movie_name": "Movie 3",
-        "description": "Movie 3 description",
-        "image": "3rdMovie.jpg",
-        "homepage": False,
-    },
-    {
-        "id": 4,
-        "movie_name": "Movie 4",
-        "description": "Movie 4 description",
-        "image": "4thMovie.jpg",
-        "homepage": False,
-    },
-]
-
-
 def home(request):
     data = {
         "categories": Category.objects.all(),
